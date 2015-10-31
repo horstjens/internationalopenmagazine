6dc7f1d061c663005b93bcdc7219b90cca936e29
Horst JENS <horstjens@gmail.com>
2015-10-31 22:50:51 +0100

    added bootstrap3 theme

---
+++ content/pages/podcastfeed.md
@@ -0,0 +1,7 @@
+Title: podcastfeed
+Date: 2015-10-26 19:00
+Modified: 2015-10-26 19:00
+
+
+getting the **international open podcast** intto your podcatcher or [feedreader](https://en.wikipedia.org/wiki/News_aggregator): coming soon, no podcastfeed xml made yet
+
---
+++ horsttagcounter.py
@@ -0,0 +1,38 @@
+import os
+
+tagger = { "teaching" : 0,
+           "coding": 0,
+           "making": 0,
+           "review": 0,
+           "report": 0
+        }
+        
+
+
+for root, dirs, files in os.walk("content", topdown=False):
+    for name in files:
+#       print( root, name)
+        if ".md" in name or ".rst" in name or ".html" in name:
+            f =open(os.path.join(root, name))
+            lines = f.readlines()
+            # is it a draft:
+            draft = False
+            for line in lines:
+                if "status:" in line[:7] and "draft" in line[7:]:
+                    draft = True
+            if draft:
+                continue
+            for line in lines:
+                if "tags:" in line[:5].lower():
+                    for tag in tagger:
+                        if tag in line[5:].lower():
+                            tagger[tag] += 1
+            f.close()
+#    for name in dirs:
+#        print(os.path.join(root, name))
+        
+print("fertig")
+print("--------------")
+
+for tag in tagger:
+    print( tag.upper(),"=", tagger[tag])
